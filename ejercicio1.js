e1cd5b344e09e9aaae07975b1e4143e72a39c2dc
JesusDGM <109067515+JesusDGM@users.noreply.github.com>
2025-08-04 16:09:24 +0200

    Create ejercicio1.js

---
+++ ejercicio1.js
@@ -0,0 +1,11 @@
+miSuperheroeFavorito = "Hulk"
+numeroFavorito = 50
+altura = 5
+longitud = 10
+suma = altura + longitud
+
+print(miSuperheroeFavorito)
+print(numeroFavorito)
+print(altura)
+print(longitud)
+print(suma)
