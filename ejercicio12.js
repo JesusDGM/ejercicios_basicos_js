32b35151cbec6dd097705af9ddf76ca9d3087e3f
JesusDGM <109067515+JesusDGM@users.noreply.github.com>
2025-08-04 16:23:50 +0200

    Create ejercicio12.js

---
+++ ejercicio12.js
@@ -0,0 +1,23 @@
+const duplicates = [
+  'sushi',
+  'pizza',
+  'burger',
+  'potatoe',
+  'pasta',
+  'ice-cream',
+  'pizza',
+  'chicken',
+  'onion rings',
+  'pasta',
+  'soda'
+];
+
+function removeDuplicates(list) {
+  const unique = [];
+  for (let i = 0; i < list.length; i++) {
+    if (!unique.includes(list[i])) {
+      unique.push(list[i]);
+    }
+  }
+  return unique;
+}
